c9ece31a2a68817e7fc797ec7c2809cd0c0e035e
Thomas von Deyen <thomas@vondeyen.com>
2020-06-23 17:41:32 +0200

    Fix picture_thumbs migration for Rails 4.2
    The foreign_key option is not supported in Rails 4.2

--- db/migrate/1_create_alchemy_picture_thumbs.rb
+++ db/migrate/1_create_alchemy_picture_thumbs.rb
@@ -3,10 +3,11 @@
 class CreateAlchemyPictureThumbs < ActiveRecord::Migration
   def change
     create_table :alchemy_picture_thumbs do |t|
-      t.references :picture, foreign_key: { to_table: :alchemy_pictures }, null: false
+      t.integer :picture_id, null: false, index: true
       t.string :signature, null: false
       t.text :uid, null: false
     end
+    add_foreign_key :alchemy_picture_thumbs, :alchemy_pictures, column: :picture_id
     add_index :alchemy_picture_thumbs, :signature, unique: true
     add_index :alchemy_picture_thumbs, :uid, unique: true
   end
