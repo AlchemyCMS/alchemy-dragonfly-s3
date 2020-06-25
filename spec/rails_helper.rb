b9ffca588175add08aceb8f4b299b0679b1f2f90
Thomas von Deyen <thomas@vondeyen.com>
2020-06-26 00:26:58 +0200

    Backport picture_thumb specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,14 @@ require File.expand_path("./dummy/config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "factory_bot"
+require "shoulda-matchers"
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
