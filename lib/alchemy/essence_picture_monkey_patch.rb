77a465bdf88cd9c874cb165b6c899c885acacdcb
Thomas von Deyen <thomas@vondeyen.com>
2020-07-06 12:19:14 +0200

    Do not enable image cropper if file is missing

--- lib/alchemy/essence_picture_monkey_patch.rb
+++ lib/alchemy/essence_picture_monkey_patch.rb
@@ -10,6 +10,10 @@ module Alchemy
       super || "alchemy/missing-image.svg"
     end
 
+    def allow_image_cropping?(options = {})
+      super && !!picture.image_file
+    end
+
     EssencePicture.prepend(self)
   end
 end
