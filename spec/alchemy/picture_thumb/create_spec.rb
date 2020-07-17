6a79e64c5d63e7c1f5f43b896c462273da618c22
Thomas von Deyen <thomas@vondeyen.com>
2020-07-17 22:58:51 +0200

    Prevent db race conditions while creating thumbs
    Create the PictureThumb record before we process and upload
    the image to S3. This helps to prevent db race conditions if
    several request hit the same image url and a thumbnail does
    not exist yet.
    
    In order to be able to retry processing an image that was not
    succesfully processed and stored on S3 we ensure to destroy the
    thumbnail on processing or connection errors.

--- spec/alchemy/picture_thumb/create_spec.rb
+++ spec/alchemy/picture_thumb/create_spec.rb
@@ -12,9 +12,37 @@ RSpec.describe Alchemy::PictureThumb::Create do
     Fog.mock!
   end
 
+  subject do
+    Alchemy::PictureThumb::Create.call(variant, "1234", "/foo/baz")
+  end
+
   it "creates thumb on picture thumbs collection" do
-    expect {
-      Alchemy::PictureThumb::Create.call(variant, "1234", "/foo/baz")
-    }.to change { variant.picture.thumbs.length }.by(1)
+    expect { subject }.to change { variant.picture.thumbs.length }.by(1)
+  end
+
+  context "on processing errors" do
+    before do
+      variant
+      expect(variant).to receive(:image) do
+        raise(Dragonfly::Job::Fetch::NotFound)
+      end
+    end
+
+    it "destroys thumbnail" do
+      expect { subject }.to_not change { variant.picture.thumbs.reload.length }
+    end
+  end
+
+  context "on connection errors" do
+    before do
+      variant
+      expect_any_instance_of(Dragonfly::Content).to receive(:store) do
+        raise(Excon::Error::Timeout)
+      end
+    end
+
+    it "destroys thumbnail" do
+      expect { subject }.to_not change { variant.picture.thumbs.reload.length }
+    end
   end
 end
