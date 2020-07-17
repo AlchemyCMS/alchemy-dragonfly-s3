58d9ec519194528c81a07488562fb07874d7aca5
Thomas von Deyen <thomas@vondeyen.com>
2020-07-17 22:58:51 +0200

    Prevent db race conditions while creating thumbs
    Create the PictureThumb record before we process and upload
    the image to S3. This helps to prevent db race conditions if
    several request hit the same image url and a thumbnail does
    not exist yet.
    
    In order to be able to retry processing an image that was not
    succesfully processed and stored on S3 we ensure to destroy the
    thumbnail on processing or connection errors.

--- app/models/alchemy/picture_thumb/create.rb
+++ app/models/alchemy/picture_thumb/create.rb
@@ -5,13 +5,23 @@ module Alchemy
     #
     class Create
       def self.call(variant, signature, uid)
-        image = variant.image
-        image.store(path: uid)
-        variant.picture.thumbs.create!(
+        # create the thumb before uploading
+        # to prevent db race conditions
+        thumb = variant.picture.thumbs.create!(
           picture: variant.picture,
           signature: signature,
           uid: uid,
         )
+        begin
+          # fetch and process the image
+          image = variant.image
+          # upload the processed image
+          image.store(path: uid)
+        rescue RuntimeError, Excon::Error => e
+          Rails.logger.warn(e)
+          # destroy the thumb if processing or upload fails
+          thumb.destroy
+        end
       end
     end
   end
