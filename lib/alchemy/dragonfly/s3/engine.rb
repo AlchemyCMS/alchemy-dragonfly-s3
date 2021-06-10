702262109cb38e383907a838ef2b0ae2371b4939
Antonio Facciolo <afdev82@gmail.com>
2021-06-10 13:25:36 +0200

    conditionally set classes

--- lib/alchemy/dragonfly/s3/engine.rb
+++ lib/alchemy/dragonfly/s3/engine.rb
@@ -7,9 +7,12 @@ module Alchemy
         engine_name "alchemy_dragonfly_s3"
 
         config.to_prepare do
-          Alchemy::Attachment.url_class = Alchemy::Attachment::S3Url
-          Alchemy::Picture.url_class = Alchemy::Picture::S3Url
-          Alchemy::PictureThumb.generator_class = Alchemy::Dragonfly::S3::CreatePictureThumb
+          Alchemy::Attachment.url_class = Alchemy::Attachment::S3Url if ::Dragonfly.app(:alchemy_attachments).datastore.is_a? ::Dragonfly::S3DataStore
+
+          if ::Dragonfly.app(:alchemy_pictures).datastore.is_a? ::Dragonfly::S3DataStore
+            Alchemy::Picture.url_class = Alchemy::Picture::S3Url
+            Alchemy::PictureThumb.generator_class = Alchemy::Dragonfly::S3::CreatePictureThumb
+          end
         end
       end
     end
