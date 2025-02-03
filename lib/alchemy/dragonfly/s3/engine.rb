b3482e411dd350415a06478693ceff3b0eb5510c
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2025-02-03 12:08:53 +0100

    Configure the engine after initialize
    When the code inside to_prepare is executed the Alchemy.user_class is nil and it fails when called to inizialize the Alchemy::Models

--- lib/alchemy/dragonfly/s3/engine.rb
+++ lib/alchemy/dragonfly/s3/engine.rb
@@ -6,7 +6,7 @@ module Alchemy
       class Engine < ::Rails::Engine
         engine_name "alchemy_dragonfly_s3"
 
-        config.to_prepare do
+        config.after_initialize do
           Alchemy::Attachment.url_class = Alchemy::Attachment::S3Url
           Alchemy::Picture.url_class = Alchemy::Picture::S3Url
           Alchemy::PictureThumb.storage_class = Alchemy::Dragonfly::S3::Store
